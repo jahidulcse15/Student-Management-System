70829681c004a52faf38bd6359dc4c5c064c3aac
Md Jahidul Islam <jahidulcse15@gmail.com>
2026-06-04 18:12:05 +0600

    Updated dashboard and about page

--- Student Management System/Controllers/HomeController.cs
+++ Student Management System/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Student_Management_System.Data;
 using Student_Management_System.Models;
 
 namespace Student_Management_System.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        /*private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        }*/
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var student = _context.Departments.Include(d=>d.Student).ToList();
+            ViewBag.StudentList = _context.Students.Include(s => s.Department).OrderByDescending(s=>s.Id).Take(5).ToList();
+            ViewBag.DepartmentStudent = _context.Students.Include(d => d.Department).ToList();
+            ViewBag.AllStudentList = _context.Students.ToList();
+            return View(student);
         }
 
         public IActionResult Privacy()
--- Student Management System/obj/Debug/net9.0/Student Management System.AssemblyInfo.cs
+++ Student Management System/obj/Debug/net9.0/Student Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f7b07d3f63b59040499f61dcfc707d3983b350")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120cefcea42359400b9419d5d34d94d4b0b35757")]
 [assembly: System.Reflection.AssemblyProductAttribute("Student Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
